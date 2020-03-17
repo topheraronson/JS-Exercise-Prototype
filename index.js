be78850141425ac1ab1fc0cf1668a3dcae7e2cf5
Christopher Aronson <christopheraronson@icloud.com>
2020-03-17 14:27:28 -0700

    Finished Person challenge

--- index.js
+++ index.js
@@ -39,9 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(someFood) {
+
+  if(this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
+
+const person = new Person('Charles', 25);
+person.eat('apple');
 
 /*
   TASK 2
